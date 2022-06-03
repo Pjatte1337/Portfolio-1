36405ec207a45416113a4bd2204c50376237161e
Pjatte <91551936+Pjatte1337@users.noreply.github.com>
2022-06-03 16:25:48 +0200

    update

--- js/app.js
+++ js/app.js
@@ -3,18 +3,18 @@ const menuToggleButton = document.querySelector(".menu-toggle-button");
 const menuElement = document.querySelector(".menu");
 
 const toggleMenu = () => {
-    menuElement.classList.toggle("active");
-    menuToggleButton.classList.toggle("active");
+  menuElement.classList.toggle("active");
+  menuToggleButton.classList.toggle("active");
 };
 menuToggleButton.addEventListener("click", toggleMenu);
 
 //remove active class
-const removeActive = e => {
-    if(e.target.classList.contains("list-link")){
-        menuElement.classList.remove("active");
-        menuToggleButton.classList.remove("active");
-    }
-}
+const removeActive = (e) => {
+  if (e.target.classList.contains("list-link")) {
+    menuElement.classList.remove("active");
+    menuToggleButton.classList.remove("active");
+  }
+};
 document.addEventListener("click", removeActive);
 
 //theme
@@ -22,17 +22,17 @@ const themeToggleButton = document.querySelector(".theme-toggle-button");
 const body = document.body;
 const current = localStorage.getItem("darkTheme");
 
-if(current) {
-    body.classList.add("dark-theme");
-};
+if (current) {
+  body.classList.add("dark-theme");
+}
 
 const toggleTheme = () => {
-    body.classList.toggle("dark-theme");
+  body.classList.toggle("dark-theme");
 
-    if(body.classList.contains("dark-theme")){
-        localStorage.setItem("darkTheme", "active");
-    }else{
-        localStorage.removeItem("darkTheme");
-    }
+  if (body.classList.contains("dark-theme")) {
+    localStorage.setItem("darkTheme", "active");
+  } else {
+    localStorage.removeItem("darkTheme");
+  }
 };
 themeToggleButton.addEventListener("click", toggleTheme);
